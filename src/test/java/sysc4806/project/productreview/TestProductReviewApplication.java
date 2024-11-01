b00cf9ad53d29393f14f66a4e0a4f821d395ef11
lubbree <lubbree@gmail.com>
2024-11-01 14:04:43 -0400

    Setup

---
+++ src/test/java/sysc4806/project/productreview/TestProductReviewApplication.java
@@ -0,0 +1,11 @@
+package sysc4806.project.productreview;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestProductReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ProductReviewApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
