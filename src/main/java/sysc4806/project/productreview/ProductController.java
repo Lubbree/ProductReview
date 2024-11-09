6f1131a1e3aba8d76c83b46fd2490e9881944a14
Bella Villeneuve <anabellavilleneuve@cmail.carleton.ca>
2024-11-08 19:28:15 -0500

    Added product page and updated Product controller with new mapping

--- src/main/java/sysc4806/project/productreview/ProductController.java
+++ src/main/java/sysc4806/project/productreview/ProductController.java
@@ -3,6 +3,7 @@ package sysc4806.project.productreview;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
@@ -18,5 +19,11 @@ public class ProductController {
     public String home(Model model) {
         model.addAttribute("products", productRepository.findAll());
         return "home";
+    }
+
+    @GetMapping("/home/{productName}")
+    public String viewProduct(@PathVariable String productName, Model model) {
+        model.addAttribute("productName", productName);
+        return "product";
     }
 }
